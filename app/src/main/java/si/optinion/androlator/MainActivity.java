27fde76d640c5fb9f71e429b2247037eced510b1
Mario Munda <mario.munda@air-tv.net>
2017-03-28 14:15:57 +0200

    Implemented plus and minus operators

--- app/src/main/java/si/optinion/androlator/MainActivity.java
+++ app/src/main/java/si/optinion/androlator/MainActivity.java
@@ -9,15 +9,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private enum operation
+    {
+      operationNone, operationSum, operationDiff
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         operand1 = 0;
-        operator = 0;
+        operator = operation.operationNone;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // What happens when button '9' is clicked
         Button but9 = (Button)findViewById(R.id.but9);
         but9.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '8' is clicked
         Button but8 = (Button)findViewById(R.id.but8);
         but8.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '7' is clicked
         Button but7 = (Button)findViewById(R.id.but7);
         but7.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '6' is clicked
         Button but6 = (Button)findViewById(R.id.but6);
         but6.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '5' is clicked
         Button but5 = (Button)findViewById(R.id.but5);
         but5.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '4' is clicked
         Button but4 = (Button)findViewById(R.id.but4);
         but4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '3' is clicked
         Button but3 = (Button)findViewById(R.id.but3);
         but3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '2' is clicked
         Button but2 = (Button)findViewById(R.id.but2);
         but2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '1' is clicked
         Button but1 = (Button)findViewById(R.id.but1);
         but1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when button '0' is clicked
         Button but0 = (Button)findViewById(R.id.but0);
         but0.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // What happens when 'CE' button is clicked
         Button butce = (Button)findViewById(R.id.butce);
         butce.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,47 +123,109 @@ public class MainActivity extends AppCompatActivity {
                 // Clear operand1
                 operand1 = 0;
                 // Clear operator
-                operator = 0;
+                operator = operation.operationNone;
             }
         });
 
+        // What happens when '+' button is clicked
         Button butplus = (Button)findViewById(R.id.butplus);
         butplus.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                // Calculate sum of two numbers (if possible)
+                // This is needed because we can click + instead of =
+                // to continue the calculation.
+                calculateSum();
+
                 // Get number from textbox and save it as operand 1
                 operand1 = getNumber();
                 // Mark operator +
-                operator = 1;
+                operator = operation.operationSum;
                 // Set textview to 0
                 setNumber(0);
             }
         });
 
+        // What happens when '-' button is clicked
+        Button butminus = (Button)findViewById(R.id.butminus);
+        butminus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // Calculate difference of two numbers (if possible)
+                // This is needed because we can click - instead of =
+                // to continue the calculation.
+                calculateDifference();
+
+                // Get number from textbox and save it as operand 1
+                operand1 = getNumber();
+                // Mark operator +
+                operator = operation.operationDiff;
+                // Set textview to 0
+                setNumber(0);
+            }
+        });
+
+        // Whan happens when '=' button is clicked
         Button butcalculate = (Button)findViewById(R.id.butcalculate);
         butcalculate.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View V){
-                // Process calculation only if operator > 0
-                if (operator == 1)
-                {
-                    // Get number from the textbox
-                    Integer num2 = getNumber();
+                // Calculate sum if possible
+                calculateSum();
 
-                    // TODO Sum two numbers
-                    Integer sum = operand1+num2;
-
-                    // Save number in the textivew
-                    setNumber(sum);
-
-                    // Reset operand1
-                    operand1 = 0;
-                    // Reset operator
-                    operator = 0;
-                }
+                // Calculate diff is possible
+                calculateDifference();
             }
         });
 
+    }
+
+    private boolean calculateDifference()
+    {
+        if (operator == operation.operationDiff)
+        {
+            // Get number from the textbox
+            Integer num2 = getNumber();
+
+            // Calculate two number difference
+            Integer diff = operand1-num2;
+
+            // Save number in the textivew
+            setNumber(diff);
+
+            // Reset operand1
+            operand1 = 0;
+            // Reset operator
+            operator = operation.operationNone;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean calculateSum()
+    {
+        if (operator == operation.operationSum)
+        {
+            // Get number from the textbox
+            Integer num2 = getNumber();
+
+            // Sum two numbers
+            Integer sum = operand1+num2;
+
+            // Save number in the textivew
+            setNumber(sum);
+
+            // Reset operand1
+            operand1 = 0;
+            // Reset operator
+            operator = operation.operationNone;
+
+            return true;
+        }
+
+        return false;
 
     }
 
@@ -201,8 +279,7 @@ public class MainActivity extends AppCompatActivity {
         String new_number = getNumber().toString().concat(num.substring(0, 1));
 
         try {
-            Integer val = Integer.parseInt(new_number);
-            lbl1.append(val.toString());
+            setNumber(Integer.parseInt(new_number));
         } catch(NumberFormatException nfe) {
             // Do nothing...
         }
@@ -215,5 +292,5 @@ public class MainActivity extends AppCompatActivity {
      * Operator 0 => No operator selected
      * Operator 1 => Plus (sum)
      */
-    private Integer operator;
+    private operation operator;
 }
